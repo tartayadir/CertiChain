f1fa627ac6945462aad09588ba8e5f6117bd6797
Yaroslav B <tartayadirhello@gmail.com>
2025-05-05 22:36:39 +0200

    Initial commit

---
+++ src/main/java/com/tartayadir/certichain/CertiChainApplication.java
@@ -0,0 +1,13 @@
+package com.tartayadir.certichain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertiChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertiChainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tartayadir/certichain/CertiChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tartayadir.certichain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
